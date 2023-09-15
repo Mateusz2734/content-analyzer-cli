7b56eae78ea7e349aa54593ff7ebe014ba14f3e8
Mateusz Wala <mateusz.wala2734@gmail.com>
2023-09-15 22:55:39 +0200

    feat: Add first (simple) working implementation

--- go.mod
+++ go.mod
@@ -4,7 +4,13 @@ go 1.21.1
 
 require (
 	github.com/olekukonko/tablewriter v0.0.5
+	github.com/urfave/cli/v2 v2.25.7
 	golang.org/x/exp v0.0.0-20230905200255-921286631fa9
 )
 
-require github.com/mattn/go-runewidth v0.0.9 // indirect
+require (
+	github.com/cpuguy83/go-md2man/v2 v2.0.2 // indirect
+	github.com/mattn/go-runewidth v0.0.9 // indirect
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20201216005158-039620a65673 // indirect
+)
--- main.go
+++ main.go
@@ -1,4 +1,39 @@
 package main
 
+import (
+	"io"
+	"log"
+	"os"
+
+	"github.com/urfave/cli/v2"
+)
+
 func main() {
+	app := &cli.App{
+		HelpName: "content-analyzer",
+		Usage:    "Analyze word frequency and letter count of a file piped from stdin",
+		Action: func(cCtx *cli.Context) error {
+			letterCounter := &LetterCount{pretty: true}
+			wordFreq := &WordFrequency{pretty: true}
+
+			stdin, err := io.ReadAll(os.Stdin)
+
+			if err != nil {
+				panic("Error: cannot get stdin")
+			}
+
+			letterCounter.data, wordFreq.data = Analyze(stdin)
+
+			letterCounter.calcSum()
+			letterCounter.render()
+
+			wordFreq.render()
+			return nil
+		},
+	}
+
+	if err := app.Run(os.Args); err != nil {
+		log.Fatal(err)
+	}
+
 }
